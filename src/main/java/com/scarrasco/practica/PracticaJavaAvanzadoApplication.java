a7cb3512b066e1af4bf0c13e9fb2cce813470341
scarrasco85 <chechucarrasco@gmail.com>
2021-02-26 13:33:30 +0100

    Initial commit

---
+++ src/main/java/com/scarrasco/practica/PracticaJavaAvanzadoApplication.java
@@ -0,0 +1,13 @@
+package com.scarrasco.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJavaAvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaJavaAvanzadoApplication.class, args);
+	}
+
+}
